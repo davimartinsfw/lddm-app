7238a449c31ebcfb67950173b166407b28e44ae4
Tadleao <thiagoirmao2010@gmail.com>
2024-05-04 18:50:58 -0300

    teste

--- lddm_puc_barberapi/src/routes/user.js
+++ lddm_puc_barberapi/src/routes/user.js
@@ -71,5 +71,49 @@ router.delete("/delete/:id", async (req, res, next) => {
     res.status(500).send(e);
   }
 });
-
+router.get("/schedule/:id", async (req, res, next) => {
+  try {
+    const response = await UserController.getSchedule();
+    res.status(200).send(response)
+  } catch (e) {
+    res.status(500).send(e)}
+});
+router.get("/uschedule/:id", async (req, res, next) => {
+  try {
+    const response = await UserController.getAllSchedule();
+    res.status(200).send(response)
+  } catch (e) {
+    res.status(500).send(e)}
+});
+router.get("/bschedule/:id", async (req, res, next) => {
+  try {
+    const response = await UserController.getAllScheduleBarber();
+    res.status(200).send(response)
+  } catch (e) {
+    res.status(500).send(e)}
+});
+router.post("/schedule", async (req, res, next) => {
+  try {
+    const response = UserController.createSchedule(req.body);
+    res.status(201).send(response)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+});
+router.post("/editschedule", async (req, res, next) => {
+  try {
+    const response = UserController.updateSchedule(req.body);
+    res.status(201).send(response)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+});
+router.delete("/cancel/:id", async (req, res, next) => {
+	try {
+    const response = UserController.cancelSchedule(req);
+    res.status(200).send(response)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+});
 module.exports = { router };
